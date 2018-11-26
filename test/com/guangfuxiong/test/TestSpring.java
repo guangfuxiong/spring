58d392842cceccc08903c4431bb52b0428f7efd3
guangfuxiong <guangfuxiong@163.com>
2018-11-26 14:46:41 +0800

    去除无用的文件

--- test/com/guangfuxiong/test/TestSpring.java
+++ test/com/guangfuxiong/test/TestSpring.java
@@ -1,10 +1,12 @@
-package com.guangfuxiong.Pojo;
+package com.guangfuxiong.test;
 
+import com.guangfuxiong.Pojo.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
+
     @Test
     public void test1(){
         ApplicationContext ac = new ClassPathXmlApplicationContext("application.xml");
