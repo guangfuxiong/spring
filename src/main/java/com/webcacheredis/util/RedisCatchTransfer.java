95e3cdf14c3652945ab84ca8f7dab7e878414f83
guangfuxiong <guangfuxiong@163.com>
2019-04-11 18:49:24 +0800

    web缓存

--- src/main/java/com/webcacheredis/util/RedisCatchTransfer.java
+++ src/main/java/com/webcacheredis/util/RedisCatchTransfer.java
@@ -4,7 +4,7 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 /**
  * 实现Catch接口，创建mybatis二级缓存实现类
- * 本质上讲，就是是要利用第三方费关系型数据库（如：redis）实现缓存功能
+ * 本质上讲，就是是要利用第三方非关系型数据库（如：redis）实现缓存功能
  */
 public class RedisCatchTransfer {
 
