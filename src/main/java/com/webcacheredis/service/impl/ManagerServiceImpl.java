8b80013631cc3d4bbc812474d0f046caf3cad52a
guangfuxiong <guangfuxiong@163.com>
2019-04-12 00:43:04 +0800

    mybatis关联查询，多表映射

--- src/main/java/com/webcacheredis/service/impl/ManagerServiceImpl.java
+++ src/main/java/com/webcacheredis/service/impl/ManagerServiceImpl.java
@@ -4,16 +4,21 @@ import com.webcacheredis.bean.Manager;
 import com.webcacheredis.dao.ManagerDao;
 import com.webcacheredis.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@CacheConfig(cacheNames = "manager")
 public class ManagerServiceImpl implements ManagerService {
     @Autowired
     private ManagerDao managerDao;
 
     @Override
+    @CacheEvict(value = {"managerInfo"}, key = "'id_'+#manager.oid")
     public void addManager(Manager manager) {
         managerDao.addManager(manager);
     }
